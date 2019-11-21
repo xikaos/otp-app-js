684a351712999e7d10f105a14532ceb7171bcff6
Francisco Zanatta <desenvolvimento@touron.com.br>
2019-11-21 17:41:46 -0300

    Convert direct access to methods from store.

--- src/components/HOTPDisplay.vue
+++ src/components/HOTPDisplay.vue
@@ -5,19 +5,19 @@
                 <div class="column has-text-centered">
                     <div class="box">
                         <h4 class="title is-4 has-text-info" id="client-counter__title">Client Counter</h4>
-                        <h5 class="title is-5 has-text-info" id="client-counter__value">{{ this.store.state.client_counter }}</h5>
+                        <h5 class="title is-5 has-text-info" id="client-counter__value">{{ this.store.getClientCounter() }}</h5>
                     </div>
                 </div>
                 <div class="column"> 
                     <div class="box has-text-centered">
                         <h4 class="title is-4 has-text-danger" id="server-counter__value">Server Counter</h4>
-                        <h5 class="title is-5 has-text-danger" id="server-counter__value">{{ this.store.state.server_counter }}</h5>
+                        <h5 class="title is-5 has-text-danger" id="server-counter__value">{{ this.store.getClientCounter() }}</h5>
                     </div>
                 </div>
                 <div class="column">
                     <div class="box has-text-centered">
                         <h4 class="title is-4 has-text-danger" id="last-valid-otp__title">Last Valid OTP</h4>
-                        <h5 class="title is-5 has-text-danger" id="last-valid-otp__title">{{ this.store.state.server_hotp }}</h5>
+                        <h5 class="title is-5 has-text-danger" id="last-valid-otp__title">{{ this.store.getLastHOTP() }}</h5>
                     </div>
                 </div>  
             </div>
@@ -30,18 +30,16 @@
         name: 'HotpDisplay',
         data() {
             return {
-                store: this.$root.vstore.store,
-                last_otp: "Undefined."   
+                store: this.$root.vstore.store,   
             }
         },
-        props: ['client_counter'],
         methods: {
             pollServer(){
                 fetch('http://localhost/api/hotp/params')
                     .then((res) => {
                         return res.json().then((json) => {
-                            this.store.state.server_counter = json.counter;
-                            this.store.state.last_hotp = json.last_otp  ;
+                            this.store.setServerCounter(json.counter);
+                            this.store.setLastHOTP(json.last_otp);
                         });
                     })
             },
@@ -49,8 +47,8 @@
                 fetch('http://localhost/api/hotp/params')
                     .then((res) => {
                         return res.json().then((json) => {
-                            this.store.state.client_counter = json.counter;
-                            this.store.state.last_hotp = json.last_otp;
+                            this.store.setClientCounter(json.counter);
+                            this.store.setLastHOTP(json.last_otp);
                         });
                     })
             }
